6227fdc31e13c80cedc0e4e11ed2d6b9f6b4a52a
hl222ih <>
2014-04-30 04:33:17 +0200

    test med anslutning till firebase och hämtning av objekt

--- www/js/app.js
+++ www/js/app.js
@@ -3,20 +3,9 @@
 //If the inserted data object lacks some properties, the corresponding properties
 //will get a null value.
 function GlossUnit(data) {
-    var createArrayOfGlossSubUnits = function(arrayData) {
-        var glossSubUnits = [],
-            i = 0;
-    
-        if (arrayData) {
-            for (i; i < arrayData.Length ; i = i+1) {
-                glossSubUnits.push(new GlossSubUnit(arrayData[i]));
-            }
-        }
-        return glossSubUnits;
-    };
     this.filename = data.filename;
-    this.glossSubUnitsLeft = createArrayOfGlossSubUnits(data.glossSubUnitsLeft);
-    this.glossSubUnitsRight = createArrayOfGlossSubUnits(data.glossSubUnitsRight);
+    this.glossSubUnitsLeft = this.createArrayOfGlossSubUnits(data.glossSubUnitsLeft);
+    this.glossSubUnitsRight = this.createArrayOfGlossSubUnits(data.glossSubUnitsRight);
     this.glossText = data.glossText;
     this.glossId = data.glossId;
     this.text = data.text;
@@ -28,6 +17,17 @@ function GlossUnit(data) {
 
 GlossUnit.prototype.isPageLink = !!this.pageUrl; //returns true if pageUrl is truthy
 GlossUnit.prototype.hasModifiedText = (this.glossText !== this.text);
+GlossUnit.prototype.createArrayOfGlossSubUnits = function(arrayData) {
+        var glossSubUnits = [],
+            i = 0;
+    
+        if (arrayData) {
+            for (; i < arrayData.length ; i = i+1) {
+                glossSubUnits.push(new GlossSubUnit(arrayData[i]));
+            }
+        }
+        return glossSubUnits;
+    };
 
 function GlossSubUnit(data) {
     this.filename = data.filename;
@@ -35,7 +35,7 @@ function GlossSubUnit(data) {
     this.comment = data.comment;
 }
 
-angular.module("blissKom", ["ui.router"])
+angular.module("blissKom", ["ui.router", "firebase"])
     .config(function($stateProvider, $urlRouterProvider) {
         $urlRouterProvider.otherwise('/main');
         $stateProvider
@@ -62,7 +62,7 @@ angular.module("blissKom", ["ui.router"])
 
 //controllers
 angular.module("blissKom")
-    .controller("MainController", function($scope) { 
+    .controller("MainController", function($scope, $firebase) { 
        // $scope.init().updateNavigationPage(2);
         $scope.greeting = "hello world!";
         $scope.items = {
@@ -113,11 +113,35 @@ angular.module("blissKom")
                     pageLinkUrl: 'some state'
                 })];
             } else {
-                $scope.glossUnits = [ new GlossUnit({filename: 'du.svg'}), new GlossUnit({filename: 'du.svg'}), new GlossUnit({filename: 'du.svg'}) ];
+                $scope.glossUnits = [ new GlossUnit({filename: 'du.svg', glossSubUnitsLeft: [{filename: 'test.svg'}]}), new GlossUnit({filename: 'du.svg'}), new GlossUnit({filename: 'hej.svg'}) ];
             }
             
         };
         $scope.updateNavigationPage();
+        
+        var myBlissRef = new Firebase('https://incandescent-fire-1738.firebaseio.com/bliss');
+        var auth = new FirebaseSimpleLogin(myBlissRef, function(error, user) {
+            auth.createUser("test2@test.com", "test", function(error, user) {
+                if (!error) {
+                    console.log('User Id: ' + user.uid + ', Email: ' + user.email);
+                } else {
+                    console.log("creation failed");
+                }
+//                auth.login('password', {
+//                    email: 'test@test.com',
+//                    password: 'test'
+//                });
+
+                myBlissRef.once('value', function(dataSnapshot) {
+                    $scope.mySnapShot = dataSnapshot;
+                    $scope.blissObjects = $scope.mySnapShot.val();
+                    test();
+                    localStorage.setItem('testObject3', JSON.stringify($scope.blissObjects));
+                });
+                    
+            });
+        });
+        //$scope.blissObjects = $firebase(myBlissRef);
     });
 
     
@@ -143,4 +167,12 @@ function onBatteryStatus(info) {
     window.alert("Level: " + info.level + " isPlugged: " + info.isPlugged);
     var testdiv = document.getElementById("test");
     testdiv.innerHTML = "Testet funkade!";
-}
+};
+
+function test() {  
+//    var myBlissRef = new Firebase('https://incandescent-fire-1738.firebaseio.com');
+//    myDataRef.set({name: "testnamn", text: "testtext"});
+//    myDataRef.on('child_added', function(snapshot) {
+//       console.log(snapshot)
+//    });
+};
